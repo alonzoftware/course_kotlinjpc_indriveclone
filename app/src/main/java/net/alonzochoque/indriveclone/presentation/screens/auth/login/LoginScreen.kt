4fce7cebdebc7d8615cff4d39a3e153eb1eb002b
MacBookAir2019 <alonzo.choque@icloud.com>
2025-05-10 18:48:01 -0400

    MacBookAir2019 :: Login Basic Design

--- app/src/main/java/net/alonzochoque/indriveclone/presentation/screens/auth/login/LoginScreen.kt
+++ app/src/main/java/net/alonzochoque/indriveclone/presentation/screens/auth/login/LoginScreen.kt
@@ -1,36 +1,56 @@
 package net.alonzochoque.indriveclone.presentation.screens.auth.login
 
+import android.R.attr.label
 import android.R.attr.padding
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import net.alonzochoque.indriveclone.R
 
 @Composable
 fun LoginScreen() {
+    var email by remember {
+        mutableStateOf(value = "")
+    }
+    var password by remember {
+        mutableStateOf(value = "")
+    }
     Scaffold(
-//        contentWindowInsets = WindowInsets.navigationBars
+        contentWindowInsets = WindowInsets.navigationBars
     ) { innerPadding ->
         Box(
             modifier = Modifier
@@ -98,6 +118,84 @@ fun LoginScreen() {
                         fontSize = 30.sp,
                         fontWeight = FontWeight.Bold
                     )
+                    Image(
+                        modifier = Modifier.size(150.dp),
+                        painter = painterResource(id = R.drawable.car_white),
+                        contentDescription = ""
+                    )
+                    Text(
+                        text = "Log in",
+                        color = Color.White,
+                        fontSize = 27.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    TextField(
+                        value = email,
+                        onValueChange = {
+                            email = it
+                        },
+                        label = {
+                            Text(text = "Email")
+                        }
+                    )
+                    TextField(
+                        value = password,
+                        onValueChange = {
+                            password = it
+                        },
+                        label = {
+                            Text(text = "Password")
+                        }
+                    )
+                    Button(
+                        onClick = {
+
+                        }
+                    ) {
+                        Text(text = "Iniciar Sesion")
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Spacer(
+                            modifier = Modifier
+                                .width(25.dp)
+                                .height(1.dp)
+                                .background(Color.White)
+                        )
+                        Text(
+                            text = "O",
+                            color = Color.White, fontSize = 17.sp
+                        )
+                        Spacer(
+                            modifier = Modifier
+                                .width(25.dp)
+                                .height(1.dp)
+                                .background(Color.White)
+                        )
+
+                    }
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "No tienes cuenta?",
+                            color = Color.White,
+                            fontSize = 17.sp
+                        )
+                        Text(
+                            text = "Registrate",
+                            color = Color.White,
+                            fontSize = 17.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+
+
+                    }
                 }
             }
         }
